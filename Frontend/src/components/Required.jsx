9699d10656165a9300d23242e62aafffa630bccf
IT21305900 <100856822+IT21305900@users.noreply.github.com>
2023-05-15 23:38:53 +0530

    Revert "ITP_HR_MANAGEMENT_SYSTEM_EMPLOYEE_LEARNING_MANAGEMENT"
    This reverts commit f7a488d3b035f2b8723b8bdf07156de31d1210d4.

--- Frontend/src/components/Required.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Required = () => {
-  return (
-    <div>Required</div>
-  )
-}
-
-export default Required
--- Server/index.mjs
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
-
-import { startServer } from "./server.mjs";
-
-
-startServer();
